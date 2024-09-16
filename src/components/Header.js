2d16196c781dbbea2abf9d4f2741d039b41944f9
eewilson32 <eewilson32@gmail.com>
2024-09-16 11:12:41 -0400

    Your commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     <header>
       <nav>
         <ul>
-          <li><a href={`${baseUrl}/#home`}>Home</a></li>
+          <li><a href={`${baseUrl}`}>Home</a></li>
           <li><a href="#">FAQ & Support</a></li>
           <li><a href="#">Account Sign-Up/Login</a></li>
           <li><a href="#">Venue Policies</a></li>
