c28a8c686354d2f122eb5fb2020522252a226eba
eewilson32 <eewilson32@gmail.com>
2024-09-16 10:27:50 -0400

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Event from './pages/Event';
 import Tickets from './pages/Tickets'
