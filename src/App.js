1b600f3d8cceba91fa1eb2451e9c93ead8789f70
eewilson32 <eewilson32@gmail.com>
2024-09-15 20:57:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Event from './pages/Event';
+import Tickets from './pages/Tickets'
+import Cart from './pages/Cart';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      {/* Header will be rendered once for all routes */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event/:eventName/:eventDate" element={<Event />}/>
+        
+        {/*paths for buttons on Events.js page*/}
+        <Route path="/home" element={<Home />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/cart" element={<Cart />} />
+
+      </Routes>
+      {/* Footer will be rendered once for all routes */}
+      <Footer />
+
+
+    </Router>
+  );
+}
+
+export default App;
